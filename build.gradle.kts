2931f4b8452a530e61336a724960f3609858293f
Lightning-64 <averyg8833@gmail.com>
2026-02-06 16:21:06 -0500

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ modstitch {
     metadata {
         modId = "tide"
         modName = "Tide"
-        modVersion = "2.0.2"
+        modVersion = "2.0.3"
         modGroup = "com.li64.tide"
         modAuthor = "Lightning64"
         modDescription = "Expands the fishing system and adds 100+ new fish."
